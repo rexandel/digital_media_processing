d690f5e1409f009003ee4b2921e0651382fe3d22
rexandel <xarendel@gmail.com>
2025-09-11 23:23:58 +0300

    Task 1.4 (Add video copying from source to output file)

--- image_video_io_testing/main.py
+++ image_video_io_testing/main.py
@@ -8,6 +8,12 @@ video = cv2.VideoCapture(os.path.join(folder, 'rick_roll.mp4'), cv2.CAP_ANY)
 
 fps = video.get(cv2.CAP_PROP_FPS)
 
+w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+video_writer = cv2.VideoWriter(os.path.join(folder, "rick_roll_grayscale.mov"), fourcc, fps, (w, h), isColor=False)
+
+
 while (True):
     now = time.time()
     ret, frame = video.read()
@@ -15,7 +21,6 @@ while (True):
     if not(ret):
         break
     
-    frame = cv2.resize(frame, (600, 500))
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
     cv2.imshow('frame', frame)
@@ -26,3 +31,8 @@ while (True):
     timeDiff = time.time() - now
     if (timeDiff < 1.0/(fps)):
         time.sleep(1.0/(fps) - timeDiff)
+    
+    video_writer.write(frame)
+
+video.release()
+cv2.destroyAllWindows()
