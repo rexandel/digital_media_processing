728abdfdb76fb5550adf07b26e5123111702c5e1
rexandel <xarendel@gmail.com>
2025-09-11 23:27:49 +0300

    Task 1.5 (Display image in both original and HSV color spaces)

--- image_video_io_testing/main.py
+++ image_video_io_testing/main.py
@@ -1,38 +1,18 @@
 import cv2
 import os
-import time
 
-folder = r"C:\Users\rexandel\Desktop\GitHub\digital_media_processing\image_video_io_testing\videos"
+folder = r"C:\Users\rexandel\Desktop\GitHub\digital_media_processing\image_video_io_testing\images"
 
-video = cv2.VideoCapture(os.path.join(folder, 'rick_roll.mp4'), cv2.CAP_ANY)
+print(folder)
 
-fps = video.get(cv2.CAP_PROP_FPS)
+img = cv2.imread(os.path.join(folder, 'cool_cat.png'))
+img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-video_writer = cv2.VideoWriter(os.path.join(folder, "rick_roll_grayscale.mov"), fourcc, fps, (w, h), isColor=False)
+cv2.namedWindow('cool_cat_rgb', cv2.WINDOW_NORMAL)
+cv2.imshow('cool_cat_rgb', img)
 
+cv2.namedWindow('cool_cat_hsv', cv2.WINDOW_NORMAL)
+cv2.imshow('cool_cat_hsv', img_hsv)
 
-while (True):
-    now = time.time()
-    ret, frame = video.read()
-
-    if not(ret):
-        break
-    
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-
-    cv2.imshow('frame', frame)
-    
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
-    
-    timeDiff = time.time() - now
-    if (timeDiff < 1.0/(fps)):
-        time.sleep(1.0/(fps) - timeDiff)
-    
-    video_writer.write(frame)
-
-video.release()
+cv2.waitKey(0)
 cv2.destroyAllWindows()
