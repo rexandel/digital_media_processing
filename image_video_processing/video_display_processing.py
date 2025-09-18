5d5082e8072b21f4d67ae7f31d3b8399de96a671
rexandel <xarendel@gmail.com>
2025-09-19 02:16:06 +0300

    Task 1.3 (Experiment with VideoCapture methods & video formats)

---
+++ image_video_processing/video_display_processing.py
@@ -0,0 +1,21 @@
+import cv2
+
+video = cv2.VideoCapture(r"C:\Users\rexandel\Documents\GitHub\digital_media_processing\image_video_processing\videos\ronaldo.mp4", cv2.CAP_ANY)
+
+while(True):
+    ret, frame = video.read()
+    if not (ret):
+        break
+
+    frame = cv2.resize(frame, (800, 500))
+
+    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    cv2.imshow('Grayscale', grayscale)
+    cv2.imshow('HSV', hsv)
+
+    if cv2.waitKey(3) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
