6cfbefccfc6b5ff6ed33d8981a984995c5618b7f
rexandel <xarendel@gmail.com>
2025-09-25 22:37:55 +0300

    Task 2.2 (Filter red color using inRange function)

---
+++ object_tracking/color_filtering.py
@@ -0,0 +1,58 @@
+import cv2
+
+
+def update_image(frame):
+    LowH = cv2.getTrackbarPos('H_MIN', 'Control')
+    HighH = cv2.getTrackbarPos('H_MAX', 'Control')
+    LowS = cv2.getTrackbarPos('S_MIN', 'Control')
+    HighS = cv2.getTrackbarPos('S_MAX', 'Control')
+    LowV = cv2.getTrackbarPos('V_MIN', 'Control')
+    HighV = cv2.getTrackbarPos('V_MAX', 'Control')
+    
+    imgThresholded = cv2.inRange(frame, (LowH, LowS, LowV), (HighH, HighS, HighV))
+    cv2.imshow('Thresholded', imgThresholded)
+
+
+def main():
+    video = cv2.VideoCapture(0)
+
+    cv2.namedWindow('Control', cv2.WINDOW_NORMAL)
+
+    LowH = 0
+    HighH = 100
+    LowS = 50
+    HighS = 255
+    LowV = 200
+    HighV = 255
+
+    cv2.createTrackbar('H_MIN', 'Control', LowH, 255, update_image)
+    cv2.createTrackbar('H_MAX', 'Control', HighH, 255, update_image)
+    cv2.createTrackbar('S_MIN', 'Control', LowS, 255, update_image)
+    cv2.createTrackbar('S_MAX', 'Control', HighS, 255, update_image)
+    cv2.createTrackbar('V_MIN', 'Control', LowV, 255, update_image)
+    cv2.createTrackbar('V_MAX', 'Control', HighV, 255, update_image)
+
+    while True:
+        ret, frame = video.read()
+        if not (ret):
+            break
+
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV_FULL)
+
+        update_image(hsv)
+
+        cv2.namedWindow('Original', cv2.WINDOW_NORMAL)
+        cv2.imshow('Original', frame)
+
+        cv2.namedWindow('HSV', cv2.WINDOW_NORMAL)
+        cv2.imshow('HSV', hsv)
+
+        if cv2.waitKey(3) & 0xFF == 27:
+            break
+    
+    video.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
